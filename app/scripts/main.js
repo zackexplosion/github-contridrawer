d5c4224efe96c2d0dee83dee8947fb35eee01a16
Tony's Hackintosh <cstony0917@gmail.com>
2015-06-19 21:21:36 +0800

    adjust function socpe

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,39 +11,41 @@ function getPixel(imagedata, x, y) {
     return {r: imagedata.data[i], g: imagedata.data[i+1], b: imagedata.data[i+2], a: imagedata.data[i+3]};
 }
 
-var git_size = {
-    width  : 31,
-    height : 7
-}
-      window.onload = function() {
-          
-          // var ctx = c.getContext("2d");
-          // // debugger;
-          var img = document.getElementById("scream");
+window.onload = function() {
+    var git_size = {
+        width  : 0,
+        height : 7
+    }
+    // var ctx = c.getContext("2d");
+    // // debugger;
+    var img = document.getElementById("scream");
 
-      
-          var canvas  = document.getElementById("myCanvas");
-          canvas.width  = git_size.width  ;
-          canvas.height = git_size.height ;
-          var canvas2 = document.getElementById("myCanvas2");
+    var width_compress_rate =  img.naturalHeight / 7;
+    git_size.width = img.naturalWidth / width_compress_rate;
+
+    var canvas  = document.getElementById("myCanvas");
+    canvas.width  = git_size.width  ;
+    canvas.height = git_size.height ;
+    // var canvas2 = document.getElementById("myCanvas2");
 
 
-          var context  = canvas.getContext("2d");
+    var context  = canvas.getContext("2d");
 
-          // var context2 = canvas2.getContext("2d");
+    // var context2 = canvas2.getContext("2d");
 
-          // context2.beginPath();
+    // context2.beginPath();
 
-          context.drawImage(img, 0, 0, git_size.width, git_size.height);
-          var imagedata = context.getImageData(0, 0, canvas.width, canvas.height);
+    context.drawImage(img, 0, 0, git_size.width, git_size.height);
+    var imagedata = context.getImageData(0, 0, canvas.width, canvas.height);
 
-          // debugger;
-          var axis = [];
+    // debugger;
+    var axis = [];
+    (function(){
+        var contribution = document.getElementById('contribution');
+        for( var x = 0 ; x < imagedata.width; x++ ) {
+        var week = document.createElement('div');
 
-          var contribution = document.getElementById('contribution');
-          for( var x = 0 ; x < imagedata.width; x++ ) {
-            var week = document.createElement('div');
-            week.className = 'week week-' + x;
+        week.className = 'week week-' + x;
 
             for( var y = 0 ; y < imagedata.height; y++ ) {          
               var day = document.createElement('span');
@@ -65,6 +67,7 @@ var git_size = {
             }
 
             contribution.appendChild(week);
-          }
-   
-      };
+        }
+    })();
+
+};
