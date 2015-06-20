c88b3a2136b9fbda5d1c24a78ecfec2ef63bdf40
Tony's Hackintosh <cstony0917@gmail.com>
2015-06-20 17:49:53 +0800

    improve code for eslint

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,6 @@
+'use strict';
+/*eslint camelcase: [2, {properties: "never"}]*/
+
 // function setPixel(imagedata, x, y, r, g, b, a) {
 //     var i = (y * imagedata.width + x) * 4;
 //     imagedata.data[i++] = r;
@@ -6,30 +9,36 @@
 //     imagedata.data[i] = a;
 // }
 
+
 function getPixel(imagedata, x, y) {
     var i = (y * imagedata.width + x) * 4;
-    return {r: imagedata.data[i], g: imagedata.data[i+1], b: imagedata.data[i+2], a: imagedata.data[i+3]};
+    return {
+        r: imagedata.data[i],
+        g: imagedata.data[i + 1],
+        b: imagedata.data[i + 2],
+        a: imagedata.data[i + 3]
+    };
 }
 
-function download(filename, text) {
-  var pom = document.createElement('a');
-  pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  pom.setAttribute('download', filename);
+// function download(filename, text) {
+//   var pom = document.createElement('a');
+//   pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+//   pom.setAttribute('download', filename);
 
-  pom.style.display = 'none';
-  document.body.appendChild(pom);
+//   pom.style.display = 'none';
+//   document.body.appendChild(pom);
 
-  pom.click();
+//   pom.click();
 
-  document.body.removeChild(pom);
-}
+//   document.body.removeChild(pom);
+// }
 
-var one_day = 1000*60*60*24;
+var one_day = 1000 * 60 * 60 * 24;
 
 var git_size = {
-    width  : 0,
-    height : 7
-}
+    width: 0,
+    height: 7
+};
 
 var fileChangeHandler = function(e){
     e.stopPropagation();
@@ -51,28 +60,27 @@ var fileChangeHandler = function(e){
         var reader = new FileReader();
 
         reader.onload = (function(theFile){
-            return function(e){
-                var data = e.target.result;
+            return function(evt){
+                var data = evt.target.result;
 
                 var img = document.getElementById("import-image");
                 img.src = data;
-                
                 render_preview();
             };
         })(f);
 
         reader.readAsDataURL(f);
         // debugger;
-        
-    };
-    
-}
+
+    }
+
+};
 
 var handleDragOver = function(e){
     e.stopPropagation();
     e.preventDefault();
     // e.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
-}
+};
 
 var render_preview = function(){
     var input = document.getElementById("import-file");
@@ -87,18 +95,18 @@ var render_preview = function(){
 
     // var ctx = c.getContext("2d");
     // // debugger;
-    var img = document.getElementById("import-image");
+    var img = document.getElementById('import-image');
 
     var width_compress_rate =  img.naturalHeight / 7;
     git_size.width = img.naturalWidth / width_compress_rate;
 
-    var canvas  = document.getElementById("myCanvas");
-    canvas.width  = git_size.width  ;
-    canvas.height = git_size.height ;
+    var canvas  = document.getElementById('myCanvas');
+    canvas.width = git_size.width;
+    canvas.height = git_size.height;
     // var canvas2 = document.getElementById("myCanvas2");
 
 
-    var context  = canvas.getContext("2d");
+    var context = canvas.getContext('2d');
 
     // var context2 = canvas2.getContext("2d");
 
@@ -114,26 +122,26 @@ var render_preview = function(){
         var contribution = document.getElementById('contribution');
         // reset preview block
         contribution.innerHTML = '';
-        
+
         var start_date = new Date('2014-08-03');
 
         // var end_date = new Date( start_date.getTime() + (one_day * axis.length) );
         // var length = (end_date.getTime() - start_date.getTime()) / one_day;
 
         var i = 0;
-        for( var x = 0 ; x < imagedata.width; x++ ) {
+        for( var x = 0; x < imagedata.width; x++ ) {
             var week = document.createElement('div');
 
             week.className = 'week week-' + x;
 
-            for( var y = 0 ; y < imagedata.height; y++ ) {          
+            for( var y = 0; y < imagedata.height; y++ ) {
                 var day = document.createElement('span');
 
                 var current_date = new Date( start_date.getTime() + (one_day * i) );
                 // console.log(current_date);
 
                 // get the pixel color code
-                var pixel = getPixel(imagedata,x,y);
+                var pixel = getPixel(imagedata, x, y);
                 if(pixel.r === 0 && pixel.g === 0 && pixel.b === 0 && pixel.a === 0){
                     // transparent pixel
                     pixel = null;
@@ -149,11 +157,10 @@ var render_preview = function(){
             contribution.appendChild(week);
         }
     })();
-    
 
     (function(){
         // var a = dates;
-        var output = document.getElementById("output");
+        // var output = document.getElementById("output");
 
         var output = '';
         for (var i = dates.length - 1; i >= 0; i--) {
@@ -164,17 +171,17 @@ var render_preview = function(){
 
             // output += dates[i]
             var date = dates[i];
-            
+
             // var d   = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
             var d = date.format('YYYY-MM-DD');
             // debugger;
-            output += d + "\n";
-        };
+            output += d + '\n';
+        }
 
         // download('dates', output);
-        
+
         // debugger;
-        // var start_date = moment([2014,08,03]);        
+        // var start_date = moment([2014,08,03]);
 
         // var end_date   = start_date.subtract(axis.length,'days');
         // debugger;
@@ -183,14 +190,13 @@ var render_preview = function(){
         // var c = start_date.diff(end_date, 'days');
         // debugger;
 
-        
 
         // var start_date = new Date('2014-08-03');
         // var end_date = new Date( start_date.getTime() + (one_day * axis.length) );
         // var length = (end_date.getTime() - start_date.getTime()) / one_day;
 
-        // var for_start = parseInt( ( new Date().getTime() - start_date.getTime() ) / one_day );        
-        // var for_end  = for_start - axis.length; 
+        // var for_start = parseInt( ( new Date().getTime() - start_date.getTime() ) / one_day );
+        // var for_end  = for_start - axis.length;
 
         // // debugger;
 
@@ -204,15 +210,15 @@ var render_preview = function(){
 // date=`date -v -"$i"d +"%Y/%m/%d"`
 //     for j in {0..50}
 //     do
-//         echo $date >> message.txt 
+//         echo $date >> message.txt
 
-//         git add . 
+//         git add .
 //         git commit --date="$date" -m "$date"
 //     done
 // done
 
     })();
-}
+};
 
 window.onload = function() {
     render_preview();
